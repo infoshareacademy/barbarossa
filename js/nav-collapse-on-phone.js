0b8b3bd6d4d607ff1e776d63da3899879ba47818
Kamil Richert <kamil.richert92@gmail.com>
2017-06-25 19:04:39 +0200

    feature/FZ5BR-8-KR Refactor with using jQuery

--- js/nav-collapse-on-phone.js
+++ js/nav-collapse-on-phone.js
@@ -1,18 +1,12 @@
 (function()  {
     $(window).resize(function addAttribute() {
-            var mq = window.matchMedia("(max-width: 767px)");
-            var i;
-            if (mq.matches) {
-                for (i = 0; i <= 3; i += 1) {
-                    document.getElementsByClassName("nav-menu__item")[i].setAttribute("data-toggle", "collapse");
-                    document.getElementsByClassName("nav-menu__item")[i].setAttribute("data-target", "#navbar-collapse");
-                }
-            }
-            else {
-                for (i = 0; i <= 3; i += 1) {
-                    document.getElementsByClassName("nav-menu__item")[i].removeAttribute("data-toggle");
-                    document.getElementsByClassName("nav-menu__item")[i].removeAttribute("data-target");
-                }
-            }
+        var mq = window.matchMedia("(max-width: 767px)");
+        var $navMenuItems = $('.nav-menu__list  a');
+        if (mq.matches) {
+            $navMenuItems.attr("data-toggle", "collapse").attr("data-target", "#navbar-collapse");
+        }
+        else {
+            $navMenuItems.removeAttr("data-toggle").removeAttr("data-toggle");
+        }
     });
 }) ();
