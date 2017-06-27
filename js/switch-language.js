1516bea5d0d4121ba3a86129d25733884159eb45
krichert <kamil.richert92@gmail.com>
2017-06-27 21:11:54 +0200

    feature/FZ5BR-28-KR Text switch on click for whole page

--- js/switch-language.js
+++ js/switch-language.js
@@ -1,27 +1,31 @@
 (function()  {
-    var $linksToTranslate = $('a[data-lang]');
     var switchButton = $('a.nav-menu__item--lang');
     var polandFlag = '<img src="pictures/langPoland.png">';
     var englandFlag = '<img src="pictures/langEngland.png">';
 
-    var $previousLinksText = $linksToTranslate.text();
-
-    var arrDataLang = [];
-
-    $linksToTranslate.each(function () {
-        arrDataLang.push($(this).attr('data-lang'));
-    });
-    console.log(arrDataLang);
+    var $linksToTranslate = $('[data-lang]');
+    var inputButtonToTranslate = $('form.premiere input[type=submit]');
+    var oldLinksValue;
 
     switchButton.click( function () {
         var previousFlag = switchButton.html();
 
         if (previousFlag === polandFlag) {
-            $linksToTranslate.text($previousLinksText);
+            $linksToTranslate.each(function () {
+                oldLinksValue = $(this).html();
+                $(this).html($(this).attr('data-lang'));
+                $(this).attr('data-lang', oldLinksValue);
+                inputButtonToTranslate.val('Zapisz się teraz!'); // text of button in value
+            });
             switchButton.html(englandFlag);
         }
         else {
-            $linksToTranslate.text(arrDataLang);
+            $linksToTranslate.each(function () {
+                oldLinksValue = $(this).html();
+                $(this).html($(this).attr('data-lang'));
+                $(this).attr('data-lang', oldLinksValue);
+                inputButtonToTranslate.val('Sign up now!'); // text of button in value
+            });
             switchButton.html(polandFlag);
         }
     })
