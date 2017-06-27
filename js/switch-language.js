76a74aad2353a35aa04dd8a72b14c7632e423702
krichert <kamil.richert92@gmail.com>
2017-06-27 20:00:04 +0200

    feature/FZ5BR-28-KR Now text switch, but all of the words is added to one element

--- js/switch-language.js
+++ js/switch-language.js
@@ -1,22 +1,28 @@
 (function()  {
-    var $elementsToTranslate = $('[data-lang]');
+    var $linksToTranslate = $('a[data-lang]');
     var switchButton = $('a.nav-menu__item--lang');
     var polandFlag = '<img src="pictures/langPoland.png">';
     var englandFlag = '<img src="pictures/langEngland.png">';
 
+    var $previousLinksText = $linksToTranslate.text();
+
+    var arrDataLang = [];
+
+    $linksToTranslate.each(function () {
+        arrDataLang.push($(this).attr('data-lang'));
+    });
+    console.log(arrDataLang);
+
     switchButton.click( function () {
         var previousFlag = switchButton.html();
-        var $previousText = $elementsToTranslate.text();
 
         if (previousFlag === polandFlag) {
+            $linksToTranslate.text($previousLinksText);
             switchButton.html(englandFlag);
-            $elementsToTranslate.text($elementsToTranslate.attr('data-lang'));
         }
         else {
+            $linksToTranslate.text(arrDataLang);
             switchButton.html(polandFlag);
-            $elementsToTranslate.text($elementsToTranslate.attr('data-lang'));
         }
-
-
     })
 }) ();
