27570074ad21e15d250e2e8d09714d870e891bfc
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-06-30 12:01:45 +0200

    FZ5BR-20-GK team animations

---
+++ js/teamAnimate.js
@@ -0,0 +1,16 @@
+/**
+ * Created by grzegorzklimek on 30.06.17.
+ */
+
+$('.Klimek').hover(function(){
+    $('#team').toggleClass('background-klimek');
+});
+$('.Richert').hover(function(){
+    $('#team').toggleClass('background-richert');
+});
+$('.Ostrowska').hover(function(){
+    $('#team').toggleClass('background-ostrowska');
+});
+$('.Paczkowski').hover(function(){
+    $('#team').toggleClass('background-paczkowski');
+});
