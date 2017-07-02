52af81e3992f7c28a0b11dd183c8b2d1da8d3b7d
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-07-02 20:40:03 +0200

    FZ5BR-20-GK fly-in effect

--- js/teamFly.js
+++ js/teamFly.js
@@ -1,6 +1,12 @@
-jQuery(document).ready(function(){
-    jQuery('.team-single').addClass("hideMe").viewportChecker({
-        classToAdd: 'showMe animated fadeIn',
-        offset: 100
+$(document).ready(function test() {
+
+    var heightZajawka = $('#home').height();
+    var heightFunction = $('#function').height();
+    var heightSign = $('#sign').height();
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > (heightZajawka + heightFunction + heightSign)) {
+        $('.team-single').removeClass('animation-hidden').addClass('animation-visible');
+        } else{}
+    })
     });
-});
