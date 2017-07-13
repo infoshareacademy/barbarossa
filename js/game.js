9ad65c0260f85fdf32b568c289b18741a84b1763
krichert <kamil.richert92@gmail.com>
2017-07-13 20:51:39 +0200

    feature/FZ5BR-22-KR Now car can't go away from board, can't go into building, take passengers

--- js/game.js
+++ js/game.js
@@ -12,7 +12,8 @@
         }
 
         var gameBoard = $('<table>');
-        var size = 10;
+        var size = 30;
+        var score = 0;
 
         times(size, function () {
             var tr = $('<tr>');
@@ -26,44 +27,55 @@
         game.append(gameBoard);
         gameBoard.focus();
 
-        function startPositionOfCar () {
+        (function startPositionOfCarAndBuildings() {
             $('tr:nth-child(10) td:nth-child(1)').addClass('car');
-        }
+            $('tr:nth-child(5) td:nth-child(5)').addClass('building');
+            $('tr:nth-child(5) td:nth-child(6)').addClass('building');
+            $('tr:nth-child(3) td:nth-child(3)').addClass('passenger');
+            $('tr:nth-child(7) td:nth-child(10)').addClass('passenger');
+        })();
 
-        function moveCar () {
-            gameBoard.keydown(function (event) {
-                var lastPositionOfCar =  $(this).find('td.car');
+        gameBoard.keydown(function moveCar(event) {
+            event.preventDefault();
+            var lastPositionOfCar = $(this).find('td.car');
+            var nextPositionOfCar;
+            var whatKeyIsPressed = event.which || event.keyCode;
 
-                var whatKeyIsPressed = event.which || event.keyCode;
+            switch (whatKeyIsPressed) {
+                case 37:
+                    nextPositionOfCar = $(this).find('td.car').prev();
+                    checkPossibilityOfMove(nextPositionOfCar, lastPositionOfCar);
+                    break;
+                case 38:
+                    nextPositionOfCar = $(this).find('td.car').parent().prev().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
+                    checkPossibilityOfMove(nextPositionOfCar, lastPositionOfCar);
+                    break;
+                case 39:
+                    nextPositionOfCar = $(this).find('td.car').next();
+                    checkPossibilityOfMove(nextPositionOfCar, lastPositionOfCar);
+                    break;
+                case 40:
+                    nextPositionOfCar = $(this).find('td.car').parent().next().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
+                    checkPossibilityOfMove(nextPositionOfCar, lastPositionOfCar);
+                    break;
+            }
+        });
 
-                switch (whatKeyIsPressed) {
-                    case 37:
-                        $(this).find('td.car').prev().addClass('car');
-                        break;
-                    case 38:
-                        $(this).find('td.car').parent().prev().find(':nth-child('+ (lastPositionOfCar.index() + 1) +')').addClass('car');
-                        break;
-                    case 39:
-                        $(this).find('td.car').next().addClass('car');
-                        break;
-                    case 40:
-                        $(this).find('td.car').parent().next().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')').addClass('car');
-                        break;
-                }
-
-
-
-
+        function checkPossibilityOfMove(nextPositionOfCar, lastPositionOfCar) {
+            if (nextPositionOfCar.hasClass('building') || nextPositionOfCar.length === 0) {
+                // do nothing -> break
+            }
+            else if (nextPositionOfCar.hasClass('passenger')) {
+                nextPositionOfCar.removeClass('passenger').addClass('car');
                 lastPositionOfCar.removeClass('car');
-            });
+                score += 1;
+                console.log(score);
+            }
+            else {
+                nextPositionOfCar.addClass('car');
+                lastPositionOfCar.removeClass('car');
+            }
         }
-
-        startPositionOfCar();
-        moveCar();
-
-
-
-
 
     });
 })();
