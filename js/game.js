bb9bd6a8b3523707a3b0234bf9375f5b32832a33
Kamil Richert <kamil.richert92@gmail.com>
2017-07-16 20:39:06 +0200

    feature/FZ5BR-22-KR now when player go into bottle inverts direction

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@
         // Board
 
         var game = $('#game');
+        var IS_DRUNK = false;
 
         function times(n, callback) {
             for (var i = 0; i < n; i += 1) {
@@ -34,7 +35,6 @@
             $('tr:nth-child(10) td:nth-child(1)').addClass('car');
             $('tr:nth-child(5) td:nth-child(5)').addClass('building');
             $('tr:nth-child(5) td:nth-child(6)').addClass('building');
-
         })();
 
         // Passengers
@@ -45,22 +45,53 @@
 
                 var x = (Math.floor(Math.random() * size - 1) + 1);
                 var y = (Math.floor(Math.random() * size - 1) + 1);
-                if ($('tr:nth-child(' + x + ') td:nth-child(' + y + ')').hasClass('building') ||
-                    $('tr:nth-child(' + x + ') td:nth-child(' + y + ')').hasClass('passenger') ||
-                    $('tr:nth-child(' + x + ') td:nth-child(' + y + ')').hasClass('car')
-                ) {}
-                else {
-                    counter++;
-                $('tr:nth-child(' + x + ') td:nth-child(' + y + ')').addClass('passenger');
-                var clearClass = setInterval(function () {
-                    $('tr:nth-child(' + x + ') td:nth-child(' + y + ')').removeClass('passenger');
+                var $nextPositionOfPassenger = $('tr:nth-child(' + x + ') td:nth-child(' + y + '):not(.building):not(.passenger):not(.car):not(.bottle)');
 
-                }, 10000)
-                if (counter > 10)
-                    clearInterval(interval);
-            }
-            }, 2000)
+                // if ($nextPositionOfPassenger.hasClass('building') ||
+                //     $nextPositionOfPassenger.hasClass('passenger') ||
+                //     $nextPositionOfPassenger.hasClass('car') ||
+                //     $nextPositionOfPassenger.hasClass('bottle')
+                // ) {
+                // } // do nothing
+                if ($nextPositionOfPassenger.length) {
+                    counter++;
+                    $nextPositionOfPassenger.addClass('passenger');
+                    setTimeout(function clearClass() {
+                        $nextPositionOfPassenger.removeClass('passenger');
+                    }, 10000);
+                    if (counter > 20) {
+                        clearInterval(interval)}
+                }
+
+            }, 1000)
         })();
+
+
+        // Bottles
+
+        (function addBottles() {
+            var counter = 0;
+            var interval = setInterval(function () {
+                var x = (Math.floor(Math.random() * size - 1) + 1);
+                var y = (Math.floor(Math.random() * size - 1) + 1);
+                var $nextPositionOfBottle = $('tr:nth-child(' + x + ') td:nth-child(' + y + '):not(.building):not(.passenger):not(.car):not(.bottle)');
+
+                // if ($nextPositionOfBottle.hasClass('building') ||
+                //     $nextPositionOfBottle.hasClass('passenger') ||
+                //     $nextPositionOfBottle.hasClass('car') ||
+                //     $nextPositionOfBottle.hasClass('bottle')
+                // ) {
+                // } // do nothing
+                if ($nextPositionOfBottle.length) {
+                    counter++;
+                    $nextPositionOfBottle.addClass('bottle');
+                    if (counter > 3) {
+                        clearInterval(interval)}
+                }
+
+            }, 1000)
+        })();
+
 
         // Car
 
@@ -69,18 +100,27 @@
             var lastPositionOfCar = $(this).find('td.car');
             var nextPositionOfCar;
             var whatKeyIsPressed = event.which || event.keyCode;
+            var moveDirection = resolveDirection(whatKeyIsPressed);
 
-            switch (whatKeyIsPressed) {
-                case 37:
+            if (!moveDirection) {
+                return
+            }
+
+            if (IS_DRUNK) {
+                moveDirection = invertDirection(moveDirection);
+            }
+
+            switch (moveDirection) {
+                case 'LEFT':
                     nextPositionOfCar = $(this).find('td.car').prev();
                     break;
-                case 38:
+                case 'UP':
                     nextPositionOfCar = $(this).find('td.car').parent().prev().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
                     break;
-                case 39:
+                case 'RIGHT':
                     nextPositionOfCar = $(this).find('td.car').next();
                     break;
-                case 40:
+                case 'DOWN':
                     nextPositionOfCar = $(this).find('td.car').parent().next().find(':nth-child(' + (lastPositionOfCar.index() + 1) + ')');
                     break;
             }
@@ -97,9 +137,45 @@
                 score += 1;
                 console.log(score);
             }
+            else if (nextPositionOfCar.hasClass('bottle')) {
+                nextPositionOfCar.removeClass('bottle').addClass('car');
+                lastPositionOfCar.removeClass('car');
+                IS_DRUNK = true;
+                setTimeout(function () {
+                    IS_DRUNK = false;
+                }, 3000);
+            }
             else {
                 nextPositionOfCar.addClass('car');
                 lastPositionOfCar.removeClass('car');
+            }
+        }
+
+        function resolveDirection(whatKeyIsPressed) {
+            switch (whatKeyIsPressed) {
+                case 37:
+                    return 'LEFT';
+                case 38:
+                    return 'UP';
+                case 39:
+                    return 'RIGHT';
+                case 40:
+                    return 'DOWN';
+            }
+        }
+
+        function invertDirection(direction) {
+            if (direction === 'LEFT') {
+                return 'RIGHT'
+            }
+            if (direction === 'UP') {
+                return 'DOWN'
+            }
+            if (direction === 'RIGHT') {
+                return 'LEFT'
+            }
+            if (direction === 'DOWN') {
+                return 'UP'
             }
         }
 
