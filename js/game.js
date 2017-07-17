5cd2002ed70ea35209ced1a02a3993a1de746485
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-07-17 15:38:49 +0200

    feature/FZ5BR-37-GK game board ver.1

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@
         }
 
         var gameBoard = $('<table>');
-        var size = 10;
+        var size = 25;
         var score = 0;
 
         times(size, function () {
@@ -31,10 +31,79 @@
 
 
         (function startPositionOfCarAndBuildings() {
-            $('tr:nth-child(10) td:nth-child(1)').addClass('car');
-            $('tr:nth-child(5) td:nth-child(5)').addClass('building');
-            $('tr:nth-child(5) td:nth-child(6)').addClass('building');
+            // Samochód
+            $('tr:nth-child(10) td:nth-child(2)').addClass('car');
 
+            // Osiedle nr 1
+            $('tr:nth-child(4) td:nth-child(4)').addClass('building building-skycraper');
+            $('tr:nth-child(4) td:nth-child(5)').addClass('building building-tree');
+            $('tr:nth-child(5) td:nth-child(4)').addClass('building building-tree');
+            $('tr:nth-child(5) td:nth-child(5)').addClass('building building-skycraper');
+            $('tr:nth-child(4) td:nth-child(6)').addClass('building building-parking');
+            $('tr:nth-child(4) td:nth-child(7)').addClass('building building-parking');
+            $('tr:nth-child(4) td:nth-child(8)').addClass('building building-parking');
+            $('tr:nth-child(4) td:nth-child(9)').addClass('building building-parking--last');
+            $('tr:nth-child(5) td:nth-child(n+6):nth-child(-n+9)').addClass('building building-tree');
+
+            //Osiedle nr 2
+            $('tr:nth-child(4) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-skycraper');
+            $('tr:nth-child(5) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-skycraper');
+
+            // Osiedle nr 3
+            $('tr:nth-child(8) td:nth-child(n+5):nth-child(-n+9)').addClass('building building-grass');
+            $('tr:nth-child(9) td:nth-child(n+4):nth-child(-n+9)').addClass('building building-tree');
+            $('tr:nth-child(8) td:nth-child(4)').addClass('building building-fountain');
+
+            // Osiedle nr 4
+            $('tr:nth-child(8) td:nth-child(n+12):nth-child(-n+13)').addClass('building building-skycraper');
+            $('tr:nth-child(9) td:nth-child(n+12):nth-child(-n+13)').addClass('building building-skycraper');
+
+            // Osiedle nr 5
+            $('tr:nth-child(8) td:nth-child(n+16):nth-child(-n+17)').addClass('building building-skycraper');
+            $('tr:nth-child(9) td:nth-child(n+16):nth-child(-n+17)').addClass('building building-skycraper');
+
+            // Osiedle nr 5
+            $('tr:nth-child(8) td:nth-child(n+20):nth-child(-n+22)').addClass('building building-skycraper');
+            $('tr:nth-child(9) td:nth-child(n+20):nth-child(-n+22)').addClass('building building-skycraper');
+
+            //Osiedle nr 7
+            $('tr:nth-child(12) td:nth-child(n+4):nth-child(-n+9)').addClass('building building-skycraper');
+            $('tr:nth-child(12) td:nth-child(n+10):nth-child(-n+22)').addClass('building building-tree');
+            $('tr:nth-child(13) td:nth-child(n+4):nth-child(-n+21)').addClass('building building-grass');
+            $('tr:nth-child(13) td:nth-child(22)').addClass('building building-fountain');
+
+            //Osiedle nr 8
+            $('tr:nth-child(n+16):nth-child(-n+22) td:nth-child(4)').addClass('building building-skycraper');
+            $('tr:nth-child(n+16):nth-child(-n+22) td:nth-child(5)').addClass('building building-skycraper');
+
+            //Osiedle nr 9
+            $('tr:nth-child(n+16):nth-child(-n+22) td:nth-child(7)').addClass('building building-tree');
+            $('tr:nth-child(n+16):nth-child(-n+22) td:nth-child(8)').addClass('building building-grass');
+
+            //Osiedle nr 10
+            $('tr:nth-child(16) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-skycraper');
+            $('tr:nth-child(17) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-grass');
+            $('tr:nth-child(19) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(12)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(13)').addClass('building building-fountain');
+            $('tr:nth-child(18) td:nth-child(14)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(15)').addClass('building building-skycraper');
+            $('tr:nth-child(18) td:nth-child(16)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(17)').addClass('building building-fountain');
+            $('tr:nth-child(18) td:nth-child(18)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(19)').addClass('building building-skycraper');
+            $('tr:nth-child(18) td:nth-child(20)').addClass('building building-grass');
+            $('tr:nth-child(18) td:nth-child(21)').addClass('building building-fountain');
+            $('tr:nth-child(18) td:nth-child(22)').addClass('building building-grass');
+
+            //Osiedle nr 11
+            $('tr:nth-child(21) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-skycraper');
+            $('tr:nth-child(22) td:nth-child(n+12):nth-child(-n+22)').addClass('building building-skycraper');
+            // Trawa dookoła
+            $('tr:nth-child(n) td:nth-child(1)').addClass('building building-grass');
+            $('tr:nth-child(1) td:nth-child(n)').addClass('building building-grass');
+            $('tr:nth-child(n) td:nth-child(25)').addClass('building building-grass');
+            $('tr:nth-child(25) td:nth-child(n)').addClass('building building-grass');
         })();
 
         // Passengers
